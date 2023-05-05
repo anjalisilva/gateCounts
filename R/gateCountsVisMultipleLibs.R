2a7a8bd29f1410677b1f577c5b3f62c23bf6ac5c
Anjali Silva <a.silva@utoronto.ca>
2023-05-05 11:08:11 -0400

    Updated multiple script

--- R/gateCountsVisMultipleLibs.R
+++ R/gateCountsVisMultipleLibs.R
@@ -1,15 +1,15 @@
 # Heatmap
-dailyHeatmap <- outputDailyCounts$dailyVisitorCounts %>%
-  ggplot2::ggplot(aes(x = "",
-                      y = date,
-                      fill = log(visitorCount + 1))) +
-  ggplot2::geom_tile() +
-  ggplot2::scale_fill_gradient(low = "white", high = "red") +
-  ggplot2::theme_bw() +
-  ggplot2::labs(y = "Date",
-                x = "", # put multiple libraries here
-                fill = "Log-transformed visitor count",
-                title = paste("Log-transformed daily visitor counts for period of",
-                              range(outputDailyCounts$dailyVisitorCounts$date)[1],
-                              "to", range(outputDailyCounts$dailyVisitorCounts$date)[2]))
-
+# dailyHeatmap <- outputDailyCounts$dailyVisitorCounts %>%
+#   ggplot2::ggplot(aes(x = "",
+#                       y = date,
+#                       fill = log(visitorCount + 1))) +
+#   ggplot2::geom_tile() +
+#   ggplot2::scale_fill_gradient(low = "white", high = "red") +
+#   ggplot2::theme_bw() +
+#   ggplot2::labs(y = "Date",
+#                 x = "", # put multiple libraries here
+#                 fill = "Log-transformed visitor count",
+#                 title = paste("Log-transformed daily visitor counts for period of",
+#                               range(outputDailyCounts$dailyVisitorCounts$date)[1],
+#                               "to", range(outputDailyCounts$dailyVisitorCounts$date)[2]))
+#
